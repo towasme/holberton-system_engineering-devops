56aac420d05f18c5bd17a2619d3eda46ae321980
towasme <towasm@gmail.com>
2020-05-05 00:00:30 -0500

    csv file creation

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" using the rest api, returns info about someone """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    r = requests.get(url + 'users/{}'.format(sys.argv[1]))
+    all_users = r.json()
+    USERNAME = all_users['username']
+
+    t = requests.get(url + 'todos?userId={}'.format(sys.argv[1])).json()
+    USER_ID = t['userId']
+
+    with open("{}.csv".format(USER_ID), "w", newline="") as csv_file:
+        csv2write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for dictionary in t:
+            title = dictionary['title']
+            status = dictionary['completed']
+            csv_file.write(USER_ID, USERNAME, status, title)
