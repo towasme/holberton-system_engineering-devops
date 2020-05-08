98170f4654853cd27c39585814293d1df2641ef3
towasme <towasm@gmail.com>
2020-05-07 22:24:53 -0500

    added conditionals to see if status is correct

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,10 +10,14 @@ def recurse(subreddit, hot_list=[], after=None):
     if after is not None:
         limit['after'] = after
     url = 'https://www.reddit.com/r/'
-    subs = requests.get(url + '{}/hot.json'.format(subreddit), headers=headers,
-                        params=limit).json()
-    for i in range(len(subs['data']['children'])):
-        hot_list.append(subs['data']['children'][i]['data']['title'])
-    if subs['data']['after'] is not None:
-        recurse(subreddit, hot_list, subs['data']['after'])
-    return hot_list
+    status = requests.get(url + '{}/hot.json'.format(subreddit),
+                          headers=headers, params=limit)
+    subs = status.json()
+    if status.status_code is 200:
+        for i in range(len(subs['data']['children'])):
+            hot_list.append(subs['data']['children'][i]['data']['title'])
+        if subs['data']['after'] is not None:
+            recurse(subreddit, hot_list, subs['data']['after'])
+        return hot_list
+    else:
+        return None
