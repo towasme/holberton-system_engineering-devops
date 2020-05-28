e593e32a59e63810d905da623f0008727aff031d
towasme <towasm@gmail.com>
2020-05-28 15:08:13 -0500

    adding restart server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-# change the wrong number placed in a file
+#change the wrong number placed in a file
 
-exec { 'change number of request':
+exec {'change number of request':
   provider => shell,
   command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
+}
+
+exec {'restart':
+  provider => shell,
   command  => 'sudo service nginx restart',
 }
